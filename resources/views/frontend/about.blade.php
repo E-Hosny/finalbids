151d7379e987c69642884d374cd110a8a487f5f8
E-Hosny <ebrahimhosny511@gmail.com>
2024-10-31 22:26:06 +0300

    done about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -34,7 +34,7 @@
         <div class="row">
             <div class="col-md-5">
                 {{-- <h2>{{ session('locale')=='en' ? 'About Us' : 'من نحن' }}</h2> --}}
-                <img class="w-100 p-3" src="{{ asset('img/about/about-us.png') }}" alt="about us">
+                <img class="w-100 p-3" src="{{ asset('img/about/about.png') }}" alt="about us">
 
             </div>
 
@@ -98,16 +98,52 @@
 
 
     <div class="container mx-auto my-2  ">
-      <div class="row {{ session('locale')=='en' ? 'text-start ms-4' : 'text-end me-4' }}">
+      <div class="row gx-3 gy-2 {{ session('locale')=='en' ? 'text-start ms-4' : 'text-end me-4' }}">
         <h2 class=" fw-bold fs-1   ">{{ session('locale')=='en' ? 'Know About Our Category' : 'تعرف على فئاتنا' }}</h2>
-        <p class="my-4">{{ session('locale')=='en'?'Bronze sculptures are a common feature in art decor, known for their timeless beauty, durability, and intricate detail. Bronze is an alloy primarily consisting of copper, often combined with tin, which gives sculptures a warm, reddish-brown hue that ages beautifully, developing a natural patina over time. 
+        <p class="my-4">{{ session('locale')=='en'?'Bronze sculptures are a common feature in art decor, known for their timeless beauty, durability, and intricate detail. Bronze is an alloy primarily consisting of copper, often combined with tin, which gives sculptures a warm, reddish-brown hue that ages beautifully, developing a natural patina over time.
             ' : 'تعتبر المنحوتات البرونزية سمة شائعة في الديكور الفني، وهي معروفة بجمالها الخالد ومتانتها وتفاصيلها المعقدة. البرونز عبارة عن سبيكة تتكون أساسًا من النحاس، وغالبًا ما يتم دمجها مع القصدير، مما يمنح المنحوتات لونًا بنيًا محمرًا دافئًا يتقادم بشكل جميل، ويتطور مع مرور الوقت.'}}
         </p>
 
-        <div class="about-card col-lg-2 col-md-2 col-4  text-center py-2  ">
-            <img src="{{ asset('img/about/antiquites.png') }}" alt="">
-            <h4>{{ session('locale')=='en' ? 'Antiquities' : 'الآثار' }}</h4>
+        <div class=" col-lg-2 col-md-4 col-6 text-center   ">
+            <div class="about-card py-3 text-center ">
+                <img class="mt-2" src="{{ asset('img/about/antiquites.png') }}" alt="">
+                <h4 class="py-1  mt-2">{{ session('locale')=='en' ? 'Antiquities' : 'الآثار' }}</h4>
+            </div>
         </div>
+        <div class=" col-lg-2 col-md-4 col-6 text-center   ">
+            <div class="about-card py-4 text-center ">
+                <img class="mt-2" src="{{ asset('img/about/asian-arts.png') }}" alt="">
+                <h4 class="py-1  mt-2">{{ session('locale')=='en' ? 'Asian Arts' : 'الفنزن الآسيوية' }}</h4>
+            </div>
+        </div>
+        <div class=" col-lg-2 col-md-4 col-6 text-center   ">
+            <div class="about-card py-3 text-center ">
+                <img class="mt-2" src="{{ asset('img/about/phones.png') }}" alt="">
+                <h4 class="py-1  mt-2">{{ session('locale')=='en' ? 'Phones' : 'الهواتف' }}</h4>
+            </div>
+        </div>
+        <div class=" col-lg-2 col-md-4 col-6 text-center   ">
+            <div class="about-card py-3 text-center ">
+                <img class="mt-2" src="{{ asset('img/about/cars.png') }}" alt="">
+                <h4 class="py-1  mt-2">{{ session('locale')=='en' ? 'Cars' : 'السيارات' }}</h4>
+            </div>
+        </div>
+        <div class=" col-lg-2 col-md-4 col-6 text-center   ">
+            <div class="about-card py-3 text-center ">
+                <img class="mt-2" src="{{ asset('img/about/old-cycles.png') }}" alt="">
+                <h4 class="py-1  mt-2">{{ session('locale')=='en' ? 'Old Cycles' : 'عجلات قديمة' }}</h4>
+            </div>
+        </div>
+        <div class=" col-lg-2 col-md-4 col-6 text-center   ">
+            <div class="about-card py-3 text-center ">
+                <img class="mt-2" src="{{ asset('img/about/books.png') }}" alt="">
+                <h4 class="py-1  mt-2">{{ session('locale')=='en' ? 'Books' : 'كتب قديمة' }}</h4>
+            </div>
+        </div>
+
+
+
+
 
       </div>
 
