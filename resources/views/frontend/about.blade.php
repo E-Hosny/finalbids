e011d65da3c030367ca31909f926de493a30e3ff
test <ebrahim-hosny@ptc-ksa.net>
2024-10-31 16:39:17 +0300

    about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -29,12 +29,12 @@
 
   <section class="policy-content">
 
-    <div class="container mx-auto">
+    <div class="container mx-auto my-4">
 
         <div class="row">
             <div class="col-md-5">
                 {{-- <h2>{{ session('locale')=='en' ? 'About Us' : 'من نحن' }}</h2> --}}
-                <img class="w-100 p-3" src="{{ asset('img/about/about.png') }}" alt="about us">
+                <img class="w-100 p-3" src="{{ asset('img/about/about-us.png') }}" alt="about us">
 
             </div>
 
@@ -93,6 +93,23 @@
 
         </div>
 
+
+    </div>
+
+
+    <div class="container mx-auto my-2  ">
+      <div class="row {{ session('locale')=='en' ? 'text-start ms-4' : 'text-end me-4' }}">
+        <h2 class=" fw-bold fs-1   ">{{ session('locale')=='en' ? 'Know About Our Category' : 'تعرف على فئاتنا' }}</h2>
+        <p class="my-4">{{ session('locale')=='en'?'Bronze sculptures are a common feature in art decor, known for their timeless beauty, durability, and intricate detail. Bronze is an alloy primarily consisting of copper, often combined with tin, which gives sculptures a warm, reddish-brown hue that ages beautifully, developing a natural patina over time. 
+            ' : 'تعتبر المنحوتات البرونزية سمة شائعة في الديكور الفني، وهي معروفة بجمالها الخالد ومتانتها وتفاصيلها المعقدة. البرونز عبارة عن سبيكة تتكون أساسًا من النحاس، وغالبًا ما يتم دمجها مع القصدير، مما يمنح المنحوتات لونًا بنيًا محمرًا دافئًا يتقادم بشكل جميل، ويتطور مع مرور الوقت.'}}
+        </p>
+
+        <div class="about-card col-lg-2 col-md-2 col-4  text-center py-2  ">
+            <img src="{{ asset('img/about/antiquites.png') }}" alt="">
+            <h4>{{ session('locale')=='en' ? 'Antiquities' : 'الآثار' }}</h4>
+        </div>
+
+      </div>
 
     </div>
 
